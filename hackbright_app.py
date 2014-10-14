fe1f6c6cec2af6df06aba877e5e0d10edfc0fbd6
Hackbright Student <info@hackbrightacademy.com>
2014-10-13 18:44:04 -0700

    added new student and new project features

--- hackbright_app.py
+++ hackbright_app.py
@@ -8,6 +8,12 @@ def make_new_student(first_name, last_name, github):
     DB.execute(query, (first_name,last_name,github))
     CONN.commit()
     print "Successfully added student: %s %s" % (first_name, last_name)
+#added this, not complete
+def make_new_project(project_title, github):
+    query = """INSERT into Students values (?,?,?)"""
+    DB.execute(query, (first_name,last_name,github))
+    CONN.commit()
+    print "Successfully added student: %s %s" % (first_name, last_name)
 
 def get_student_by_github(github):
     query = """SELECT first_name, last_name, github FROM Students WHERE github = ?"""
@@ -15,6 +21,14 @@ def get_student_by_github(github):
     row = DB.fetchone()
     print """\
 Student: %s %s
+Github account: %s"""%(row[0], row[1], row[2])
+#added this, not complete
+def get_projects_by_title(project_title):
+    query = """SELECT first_name, last_name, github FROM Students WHERE github = ?"""
+    DB.execute(query, (github,))
+    row = DB.fetchone()
+    print """\
+Project Title: %s %s
 Github account: %s"""%(row[0], row[1], row[2])
 
 def connect_to_db():
@@ -27,9 +41,13 @@ def main():
     command = None
     while command != "quit":
         input_string = raw_input("HBA Database> ")
+        # print 1.0, input_string
         tokens = input_string.split()
+        # print 2.0, tokens
         command = tokens[0]
+        # print 3.0, command
         args = tokens[1:]
+        # print 4.0, args
 
         if command == "student":
             get_student_by_github(*args) 

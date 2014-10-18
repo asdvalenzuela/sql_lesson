a3c04e512e87af9942d961ac25c74d01e435d345
Hackbright Student <info@hackbrightacademy.com>
2014-10-17 18:01:31 -0700

    added form to add new student and grade and display in one page

--- hackbright_app.py
+++ hackbright_app.py
@@ -7,13 +7,15 @@ def make_new_student(first_name, last_name, github):
     query = """INSERT into Students values (?,?,?)"""
     DB.execute(query, (first_name,last_name,github))
     CONN.commit()
-    return "Successfully added student: %s %s" % (first_name, last_name)
+    return "Successfully added student: %s %s %s" % (first_name, last_name,github)
 
-def make_new_project(title, description, max_grade):
-    query = """INSERT into Projects (title, description, max_grade) values (?,?,?)"""
-    DB.execute(query, (title,description,max_grade))
+def make_new_project(title, max_grade):
+    query = """INSERT into Projects (title, max_grade) values (?,?)"""
+    DB.execute(query, (title, max_grade))
     CONN.commit()
-    return "Successfully added project %s, %s, with max grade %s" % (title, description,max_grade)
+    return "Successfully added project %s, with max grade %s" % (title, max_grade)
+
+
 
 def get_student_by_github(github):
     query = """SELECT first_name, last_name, github FROM Students WHERE github = ?"""
--- webapp.py
+++ webapp.py
@@ -7,13 +7,24 @@ app = Flask(__name__)
 def new_student():
     return render_template("new_student.html")
 
-@app.route("/projectgrades")
-def get_projectgrades():
+@app.route("/newstudentrecord")
+def add_new_student():
     hackbright_app.connect_to_db()
-    project_name = request.args.get("project")
-    row = hackbright_app.get_grades_by_project_title(project_name)
-    html = render_template("new_student_record.html", project_title = project_name, project_grades = row)
+    student_info1 = request.args.get("studentfirstname")
+    student_info2 = request.args.get("studentlastname")
+    student_info3 = request.args.get("studentgithub")
+    row = hackbright_app.make_new_student(student_info1,student_info2,student_info3)
+    html = render_template("new_student_record.html", student = row, studentfirstname= 'alaina')
     return html    
+
+@app.route("/addproject")
+def add_new_project():
+    hackbright_app.connect_to_db()
+    projectname = request.args.get("projectname")
+    projectgrade = request.args.get("projectgrade")
+    row2 = hackbright_app.make_new_project(projectname, projectgrade)
+    html = render_template("new_student_record.html", newprojectgrade = row2)
+    return html
 
 @app.route("/projectgrades")
 def get_projectgrades():
@@ -32,6 +43,7 @@ def get_student():
     html = render_template("student_info.html", 
         first_name = row[0], last_name = row[1], github = row[2], grade = row2)
     return html
+
 
 @app.route("/")
 def get_github():
